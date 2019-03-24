84bd471ea9f94a2b4c93f9070a2f549551036cc1
Sergey Kozyrev <serhii.kozyriev@gmail.com>
2019-03-24 16:18:12 +0200

    mobile version done

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-var sections = $('section')
-  , nav = $('.page-header')
-  , nav_height = nav.outerHeight() + 111;
-  
+var sections = $('section'),
+  nav = $('.page-header'),
+  nav_height = nav.outerHeight() + 111;
+
 // Подсветка пункта меню в зависимости от секции
 $(window).on('scroll', function () {
   var cur_pos = $(this).scrollTop();
   sections.each(function () {
     var top = $(this).offset().top - nav_height,
       bottom = top + $(this).outerHeight();
-
     if (cur_pos >= top && cur_pos <= bottom) {
       nav.find('.menu-link').removeClass('active');
       nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
@@ -18,8 +17,8 @@ $(window).on('scroll', function () {
 
 // Скрол к секции в зависимости от пункта меню по кторому был клик
 nav.find('.menu-link').on('click', function () {
-  var $el = $(this)
-    , id = $el.attr('href');
+  var $el = $(this),
+    id = $el.attr('href');
   $('html, body').animate({
     scrollTop: $(id).offset().top - nav_height + 111
   }, 500);
@@ -32,6 +31,7 @@ $(document).ready(function () {
   // Показать/Скрыть меню
   $(".collapse-menu").on("click", function () {
     $(this).toggleClass("is-active");
+    $(".header-menu").toggleClass("is-active");
   });
 
   // Анимация чисел при скролле
